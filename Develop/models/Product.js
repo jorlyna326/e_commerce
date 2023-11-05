6f7c97e48b2bc951af55e3348b603eb253b7c6c2
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-11-04 23:07:51 -0400

    created data base models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true, // This ensures that the value is a decimal
+      }
+    },
+    stock: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    catergory_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    }
   },
   {
     sequelize,
