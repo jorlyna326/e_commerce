f1a7ee5d22667131f6561fbc84984616d0d59ded
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-11-20 23:14:28 -0500

    finished tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,83 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+   
+    const tagData = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          through: ProductTag
+        }
+      ]
+    })
+
+    res.json(tagData)
+
+  } catch (err) {
+    res.json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id,
+      {
+        include: [
+          {
+            model: Product,
+            through: ProductTag
+          }
+      ]
+      });
+    res.json(categoryData)
+  } catch (err) {
+    res.json(err)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body)
+    res.json(newTag)
+  } catch (err) {
+    res.json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    res.json(updatedTag)
+  } catch (err) {
+    res.json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData =await Tag.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+    res.json(tagData)
+  } catch (err) {
+    res.json(err)
+  }
 });
 
 module.exports = router;
